377fd38856ac30ccd61e750136e64451135b7f97
wmaKRET <wkret91.2@gmail.com>
2022-08-02 15:09:06 +0200

    Setup ApolloClient dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react"
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from "@apollo/client"
 
 class App extends React.Component {
-   render() {
+  render() {
+    const client = new ApolloClient({
+      uri: "https://graphql-pokemon2.vercel.app/",
+      cache: new InMemoryCache()
+    })
+
     return (
-      <div>
-        <h1>hello world</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div>
+          <h1>hello world</h1>
+        </div>
+      </ApolloProvider>
     )
   }
 }
